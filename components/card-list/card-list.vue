225c9b1bf5027772e76b126063385864b15c4ea0
szz <1135304930@qq.com>
2021-04-16 09:54:10 +0800

    频道页面

---
+++ components/card-list/card-list.vue
@@ -0,0 +1,140 @@
+<template>
+	<view class="my-card" @click="open">
+		<view class="thumb">
+			<view class="flex flex-column flex-1 px-2">
+				<text class="font text-dark">{{channel.name}}</text>
+				<text class="font-sm  text-muted">刚刚更新</text>
+			</view>
+			<image :src="channel.cover" class="img" mode="aspectFill" />
+			<view class="title ml-3 multiple-line box " style="margin-left:12px">{{archive.name}}</view>
+			<view class="title1 ml-3 mt-1 text-muted">精选视频{{channel.featured_count}}个  ></view>
+		</view>
+		<view class="detail">
+			<view class="like mt-2">
+				<text class="add text-pink">{{channel.archive_count}}  订阅</text>
+			</view>
+			
+			<image :src="channel.cover" class="img" mode="aspectFill" />
+			<!-- <view>
+				<view class="video-card_info">
+					<span class="play-text">
+					<image src="../../static/image/index/biligame_video_play_count.png" />
+					<view>{{archive.view_count}}</view>
+					</span>
+					<span class="like-text">
+						<image src="../../static/image/index/ic_album_comment_count.png" />
+					</span>
+					<span class="play-duraiton">{{archive.like_count}}</span>
+				</view>
+				<view>{{archive.duration}}</view>  
+			</view> -->
+			
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+	    name: 'cardList',
+	    props: ['channel','archive'], //通过props传值，父组件中将article传过来
+	    computed: {
+	      //计算属性，文章摘要的长度超出，就用。。。代替
+	      title() {
+	        let title = this.rank.title;
+	        if (title && title.length > 12) {
+	          title = title.substring(0, 12) + '...';
+	        }
+	        return title;
+	      }
+	    },
+	    methods: {
+	      open() {
+	        console.log("click");
+	        //在子组件my-card.vue中将点击事件传回父组件index.vue
+	        this.$emit('open');
+	      }
+	    }
+	  };
+</script>
+
+<style lang="scss" scoped>
+	.box {
+		width: 160px; // 别漏了定义宽度
+	}
+	
+	.multiple-line {
+		display: -webkit-box;
+		-webkit-box-orient: vertical;
+		-webkit-line-clamp: 2;
+		overflow: hidden;
+	}
+	// .video-card_info {
+	// 	z-index: 1;
+	// 	position: absolute;
+	// 	width: 100%;
+	// 	align-items: center;
+	// 	bottom: 5px;
+	// 	font-size: 12px;
+	// 	padding:0 8px;
+	// 	display: flex;
+	// }
+	// .video-card_info .play-text {
+	// 	margin-right: 12px;
+	// }
+	// .video-card_info .like-text {
+	// 	flex: 1;
+	// } 
+	.my-card {
+		padding: 20rpx 20rpx;
+		border-bottom: 1px dashed #ddd;
+		display: flex;
+		background-color: #fff;
+		font-size: 14px;
+
+		.thumb {
+			width: 300rpx;
+			height: 500rpx;
+			position: relative;
+
+			.img {
+				width: 340rpx;
+				height: 240rpx;
+				border-radius: 10%;
+				position: absolute;
+				margin-top: 15rpx;
+				left: 10rpx;
+			}
+			.title {
+				margin-top: 120px;
+			}
+			
+		}
+
+		.detail {
+			padding-left: 92px;
+			// word-wrap: break-word;
+			width: 220rpx;
+			position: relative;
+			display: flex;
+			flex-direction: column;
+			.like {
+				height: 20px;
+				width: 190rpx;
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				border-radius: 10%;
+				border: 1px solid #FFB6C1;
+
+			}
+			.img {
+				width: 340rpx;
+				height: 240rpx;
+				border-radius: 10%;
+				position: absolute;
+				margin-top: 110rpx;
+				left: 60rpx;
+			}
+		}
+	}
+</style>
--- pages/channel/channel.vue
+++ pages/channel/channel.vue
@@ -1,48 +1,211 @@
 <template>
-<view>
-	<h2>首页</h2>
-		<view class='box'>
-			<!-- {{msg}} -->
+	<view>
+       <view class="uni-padding-wrap ">
+			<segmented-control :current="currentItem" :values="items" :style-type="'text'" :active-color="'#f3a8cb'"
+				@clickItem="onClickItem" style="margin-top: 45px;"/>
 		</view>
-		<text class='iconfont icon-shipin '></text>
-		<!-- <text v-if="true" v-html="hhh">wwww</text> -->
-		<!-- <button type="primary" @click="List">获取数据</button> -->
+		<view class="content">
+			<!-- 选项卡1的内容 -->
+			<view v-if="currentItem === 0">
+				
+				<view class="input-view mt-0" style="position: relative;">
+					<view class="iconfont icon-wddl" type="search" style="position: absolute; margin-left: 17px; margin-top: -4px;"></view>
+					<input
+						class="nav-bar-input bg-hover-light rounded-circle pl-5 ml-3 mt-2" type="text"
+						placeholder="搜索你感兴趣的频道" @click="search" v-model="keyword">
+				</view>
+				
+				<view class="top">
+						   <view class="flex flex-column" style="width: 20%;align-items: center;">
+							   <image class="img" src="../../static/image/index2/index/ic_launcher_round.png"/>
+							   <text class="text-muted">哔哩哔哩</text>
+						   </view>
+						   <view class="flex flex-column" style="width: 10%;align-items: center;">
+						   	    <image class="img" src="../../static/image/index2/index/ic_share_weibo.png"/>
+						   		<text class="text-muted">微博</text>
+						   </view>
+						   <view class="flex flex-column" style="width: 20%;align-items: center;">
+						   		<image class="img" src="../../static/image/index2/index/ic_share_wechat_moments.png"/>
+						   		<text class="text-muted">微信朋友圈</text>
+						   </view>
+						   <view class="flex flex-column" style="width: 16%;align-items: center;">
+						   		<image class="img" src="../../static/image/index2/index/ic_share_qq.png"/>
+						   		<text class="text-muted">QQ空间</text>
+						   </view>
+						   <view class="flex flex-column " style="width: 15%;align-items: center;">
+						   		<image class="img img3" src="../../static/image/index2/index/tjdy.png"/>
+						   		<text class="text-muted">添加订阅</text>
+						   </view>
+				</view>
+				
+				<view class="present">
+					<view>
+					<text class="text-muted" style="margin-left: 20rpx;">最近看过的频道</text>
+					
+					</view>
+				</view>
+				
+				
+				<view>
+					<view style="height: 30px; display: flex; flex-direction: row;">
+					<view class="text-muted mt-2" style="margin-left: 20rpx;">热门频道 </view>
+					<view class=" text-muted font-sm mt-2" style="margin-left: 450rpx;">换一换</view>
+					<view class="iconfont icon-shuaxin ml-1 " style="font-size: 23px;"></view>
+					</view>
+					<view class="top">
+							   <view class="flex flex-column" style="width: 18%;align-items: center;">
+							   	    <image class="img" src="../../static/image/index2/index/bili_socialize_sina.png"/>
+							   		<text class="text-muted">微博</text>
+							   </view>
+							   <view class="flex flex-column" style="width: 20%;align-items: center;">
+							   		<image class="img" src="../../static/image/index2/index/bili_socialize_wx_moment.png"/>
+							   		<text class="text-muted">微信朋友圈</text>
+							   </view>
+							   <view class="flex flex-column" style="width: 20%;align-items: center;">
+							   		<image class="img" src="../../static/image/index2/index/bili_socialize_wx_chat.png"/>
+							   		<text class="text-muted">微信</text>
+							   </view>
+							   <view class="flex flex-column" style="width: 20%;align-items: center;">
+							   		<image class="img" src="../../static/image/index2/index/bili_socialize_qq_chat.png"/>
+							   		<text class="text-muted">QQ</text>
+							   </view>
+							   <view class="flex flex-column" style="width: 20%;align-items: center;">
+							   		<image class="img" src="../../static/image/index2/index/bili_socialize_im.png"/>
+							   		<text class="text-muted">热聊</text>
+							   </view>
+					</view>
+					<view class="present top">
+						<image class="img2 ml-2" src="../../static/image/index2/index/bili_socialize_wx_chat.png"/>
+						<view class="flex flex-column ml-2" style="width: 30%;">
+							<text class="  font" >微信聊天</text>
+							<text class=" text-muted font-sm" >新增拍一拍功能</text>
+						</view>
+						<view class="flex justify-center align-center" style="background-color: #f3a8cb; height: 50rpx; width: 150rpx; border-radius: 10%;margin-left: 80px; ">
+							<text class="text-white">去看看</text>
+						</view>
+					</view>	
+					
+					
+					
+					
+					
+				</view>
+				
+				<view v-for="(archive,index) in archives" :key="index">
+					<view v-for="(channel,index) in channels" :key="index" >
+					
+				<card-list :channel="channel" :archive="archive"></card-list>
+				</view>
+				</view>
+			</view>
+		</view>
+
 		
-		<view v-for="(item,index) in list" :key="index">
-			<view>{{item}}</view>
-		</view>
+
 	</view>
 </template>
 
 <script>
+	import SegmentedControl from '@/components/segmented-control/segmented-control.vue'
+	import cardList from '@/components/card-list/card-list.vue'
 	export default {
+		components: {
+			SegmentedControl
+		},
 		data() {
 			return {
-			// list: [],
-			}
+				channels: [],
+				archives: [],
+				items: ['频道', '分区'],
+				currentItem: 0,
+				updateVData: '',
+				keyword: ''
+			};
 		},
-		onLoad() {
-		   
-			uni.request({
-			        url: "https://api.bilibili.com/x/web-interface/popular?ps=20&pn=1",                  
-			        success: (res) => {
-			            console.log(res);
-			        }          
-		})
-
-			uni.request({
-				url: "https://api.bilibili.com/x/web-interface/popular?ps=20&pn=1",  
-				success: (res) => {
-							console.log(res);
-							}
-		})
-		},
-
+		
 		methods: {
-			}
+
+			search() {
+				uni.navigateTo({
+					url:'../search/search'
+				})
+			},
+			onClickItem(e) {
+				if (this.currentItem !== e.currentIndex) {
+					this.currentItem = e.currentIndex
+				}
+			},
+			onLoad() {
+				uni.request({
+					url: 'https://api.bilibili.com/x/web-interface/web/channel/category/channel_arc/list?id=0',
+					success: (res) => {
+						console.log(res);
+						console.log(res.data.data.archive_channels);
+						this.channels = res.data.data.archive_channels
+						for (let i = 0; i < this.channels.length; i++) {
+							this.archives = res.data.data.archive_channels[i].archives
+							console.log(this.archives);
+						}
+			
+					}
+				});
+			},
+
+			
 		}
+
+	}
 </script>
 
-<style>
+<style scoped>
+	page {
+		height: 100%;
+	}
+    .top{
+		display: flex;
+		flex-direction: row ;
+		width: 100%;
+		align-items: center;
+		margin-top: 20rpx;
+	}
+	.img{
+		background-color: #fff;
+		width: 100rpx;
+		height: 100rpx;
+	}
+	.img2{
+		background-color: #E6E6E6;
+		margin-top: 5px;
+		margin-bottom: 5px;
+		
+		width: 80rpx;
+		height: 80rpx;
+	}
+	.img3{
+		border-radius: 100%;
+	}
+	.present{
+		margin-left: 15px;
+		margin-right: 15px;
+		border-radius: 5%;
+		background-color: #e6e6e6;
+	}
+	.uni-padding-wrap {
+		width: 700rpx;
+		height: 200rpx;
+		position: fixed;
+		top: 0;
+		z-index: 999;
+		background-color: #fff;
+		border-bottom: 1px solid #ddd;
+	}
 
+	.content {
+		position: absolute;
+		margin-top: 110px;
+		z-index: 1;
+	}
+	.nav-bar-input {
+		width: 300px;
+	}
 </style>
